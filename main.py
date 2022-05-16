6cd3d6da0a1da63882763ef4fbc669c65ffe6431
WilsonVinson <cprewilsonvinson@gmail.com>
2022-05-16 14:07:53 +0800

    main

--- main.py
+++ main.py
@@ -1,38 +1,43 @@
 
-import socket
+# import socket
 import datetime
-import geoip2.database
+# import geoip2.database
 
 import module.day_convert as day_convert
 
-ip = socket.gethostbyname(socket.gethostname())
-reader = geoip2.database.Reader('./GeoLite2-City.mmdb')
-response = reader.city(ip)
-# 有多种语言，我们这里主要输出英文和中文
-print("你查询的IP的地理位置是:")
+# ip = socket.gethostbyname(socket.gethostname())
+# reader = geoip2.database.Reader('./GeoLite2-City.mmdb')
+# response = reader.city(ip)
+# # 有多种语言，我们这里主要输出英文和中文
+# print("你查询的IP的地理位置是:")
 
-print("地区：{}({})".format(response.continent.names["es"],
-                                       response.continent.names["zh-CN"]))
+# print("地区：{}({})".format(response.continent.names["es"],
+#                                        response.continent.names["zh-CN"]))
 
-print("国家：{}({}) ，简称:{}".format(response.country.name,
-                                                        response.country.names["zh-CN"],
-                                                        response.country.iso_code))
+# print("国家：{}({}) ，简称:{}".format(response.country.name,
+#                                                         response.country.names["zh-CN"],
+#                                                         response.country.iso_code))
 
-print("洲／省：{}({})".format(response.subdivisions.most_specific.name,
-                                          response.subdivisions.most_specific.names["zh-CN"]))
+# print("洲／省：{}({})".format(response.subdivisions.most_specific.name,
+#                                           response.subdivisions.most_specific.names["zh-CN"]))
 
-print("城市：{}({})".format(response.city.name, 
-                                          response.city.names["zh-CN"]))
+# print("城市：{}({})".format(response.city.name, 
+#                                           response.city.names["zh-CN"]))
 
-print("经度：{}，纬度{}".format(response.location.longitude,
-                                            response.location.latitude))
+# print("经度：{}，纬度{}".format(response.location.longitude,
+#                                             response.location.latitude))
 
-print("时区：{}".format(response.location.time_zone))
+# print("时区：{}".format(response.location.time_zone))
 
-print("邮编:{}".format(response.postal.code))
+# print("邮编:{}".format(response.postal.code))
 
 
-
+DC = day_convert.DayConvert(datetime.datetime(2020, 5, 1, 0, 0, 0))
+DC.print()
+DC = day_convert.DayConvert(datetime.datetime(2020, 6, 1, 0, 0, 0))
+DC.print()
+DC = day_convert.DayConvert(datetime.datetime(2020, 7, 1, 0, 0, 0))
+DC.print()
 
 def dt_change():
     for year in range(2000,2022,1):
